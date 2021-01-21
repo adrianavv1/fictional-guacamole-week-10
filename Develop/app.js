2a0f08f2bd1246957e29d3159a7b4c2717578571
Adriana Valdiglesias <adrianavaldiglesias2@gmail.com>
2021-01-21 09:57:38 -0500

    still working on it...

--- Develop/app.js
+++ Develop/app.js
@@ -4,93 +4,77 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+​
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+​
 const render = require("./lib/htmlRenderer");
-const { generate } = require("rxjs");
-
+const {
+  generate
+} = require("rxjs");
+​
 const engagementTeam = [];
-
+​
 const confirmName = async (name) => {
   if (name === '') {
     return 'Incorrect name';
   };
   return true;
 };
-
+​
 const confirmNumber = async (name) => {
   if (name === '') {
     return 'Incorrect number';
   };
   return true;
 };
-
+​
 function validateEmail(name) {
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(name)) {
     return (true)
   }
   return ("You have entered an invalid email address!")
 }
-
+​
 //Gathering information about Manager.
 function addManager() {
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: "What's the name of the manager?",
-      validate: confirmName
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: "What's the id of the manager?",
-      validate: confirmNumber
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: "What's the email of the manager?",
-      validate: validateEmail
-    },
-    {
-      type: 'input',
-      name: 'officeNumber',
-      message: "What's the office number of the manager?",
-      validate: confirmNumber
-    }
-  ])
+  inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: "What's the name of the manager?",
+        validate: confirmName
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What's the id of the manager?",
+        validate: confirmNumber
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What's the email of the manager?",
+        validate: validateEmail
+      },
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What's the office number of the manager?",
+        validate: confirmNumber
+      }
+    ])
     .then((answer) => {
-      const Manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
-      engagementTeam.push(Manager);
+      const newManager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+      engagementTeam.push(newManager);
+      console.log('Manager created! ' + newManager);
       pickNextMember()
     })
     .catch(err => console.log(err));
-  console.log('Manager created! ' + newManager);
-
-  start();
 }
-
-
-const pickNextMember = () => {
+​
+//Gathering information about Engineer.
+function addEngineer() {
   inquirer.prompt([{
-    type: 'list',
-    name: 'employeeType',
-    message: "Which employee type do you want to add?",
-    choices: [
-      'Manager',
-      'Engineer',
-      'Intern',
-      'Exit',
-    ]
-  }]);
-
-  if (answer.employeeType == 'Engineer') {
-    addEngineer();
-    inquirer.prompt([
-      {
         type: "input",
         message: "What is the engineer's name?",
         name: "name",
@@ -115,34 +99,18 @@ const pickNextMember = () => {
         validate: confirmName
       },
     ])
-      .then((answer) => {
-        const Engineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
-        engagementTeam.push(Engineer);
-      })
-      .catch(err => console.log(err));
-    console.log('Engineer created! ' + newEngineer);
-
-  }
- 
+    .then((answer) => {
+      const newEngineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
+      engagementTeam.push(newEngineer);
+      console.log('Engineer created! ' + newEngineer);
+      pickNextMember()
+    })
+    .catch(err => console.log(err));
 }
-
-const start = () => {
+​
+//Gathering information about Intern.
+function addIntern() {
   inquirer.prompt([{
-    type: 'list',
-    name: 'employeeType',
-    message: "Which employee type do you want to add?",
-    choices: [
-      'Manager',
-      'Engineer',
-      'Intern',
-      'Exit'
-    ]
-  }]);
-
-  if (answer.employeeType == 'Intern') {
-    addIntern();
-    inquirer.prompt([
-      {
         type: "input",
         message: "What is your intern's name?",
         name: "name",
@@ -167,37 +135,85 @@ const start = () => {
         validate: confirmName
       }
     ])
-      .then((answer) => {
-        const Intern = new Intern(answer.name, answer.id, answer.email, answer.school);
-        engagementTeam.push(Intern);
-      })
-      .catch(err => console.log(err));
-
-    console.log('intern created! ' + newIntern);
-
-    console.log('Goodbye!')
-
-    // start writing to file! / or generating the html
-    // user render()
-  }
-  else { generateFile() }
-
-  /*       try {
-          }
-          .catch(err) {
-          console.log(err);
-        } */
-
-
-        startWrite();
-
-  //Call the function
-
-
-
-  function startWrite() {
-    fs.writeFileSync(outputPath, render(engagementTeam), "utf-8")
-  }
+    .then((answer) => {
+      const newIntern = new Intern(answer.name, answer.id, answer.email, answer.school);
+      engagementTeam.push(newIntern);
+      console.log('intern created! ' + newIntern);
+      pickNextMember()
+    })
+    .catch(err => console.log(err));
 }
-
-start();
+​
+​
+const pickNextMember = () => {
+  inquirer.prompt([{
+    type: 'list',
+    name: 'employeeType',
+    message: "Which employee type do you want to add?",
+    choices: [
+      'Manager',
+      'Engineer',
+      'Intern',
+      'Exit',
+    ]
+  }]).then((answer) => {
+​
+    if (answer.employeeType == 'Engineer') {
+      addEngineer();
+    } else if (answer.employeeType == 'Manager') {
+      addManager();
+    } else if (answer.employeeType == 'Intern') {
+      addIntern();
+    } else {
+      // start writing the team
+      console.log('Start rendering the team!')
+      // Resume coding here!!!
+      
+    }
+  })
+}
+​
+// const start = () => {
+//   inquirer.prompt([{
+//     type: 'list',
+//     name: 'employeeType',
+//     message: "Which employee type do you want to add?",
+//     choices: [
+//       'Manager',
+//       'Engineer',
+//       'Intern',
+//       'Exit'
+//     ]
+//   }]);
+​
+//   if (answer.employeeType == 'Intern') {
+//     addIntern();
+​
+​
+//     console.log('Goodbye!')
+​
+//     // start writing to file! / or generating the html
+//     // user render()
+//   } else {
+//     generateFile()
+//   }
+​
+//   /*       try {
+//           }
+//           .catch(err) {
+//           console.log(err);
+//         } */
+​
+​
+//   startWrite();
+​
+//   //Call the function
+​
+​
+​
+//   function startWrite() {
+//     fs.writeFileSync(outputPath, render(engagementTeam), "utf-8")
+//   }
+// }
+​
+pickNextMember();
